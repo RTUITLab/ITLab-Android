a9a6eaca3d7325f0f26d87c9a2c3c697cbbbb1dc
CalamityDeadshot <semyon.pyankov@gmail.com>
2021-11-14 21:48:38 +0300

    Implemented showing notifications when application is in foreground

--- app/src/main/java/ru/rtuitlab/itlab/services/firebase/AppFirebaseMessagingService.kt
+++ app/src/main/java/ru/rtuitlab/itlab/services/firebase/AppFirebaseMessagingService.kt
@@ -5,7 +5,24 @@ import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import java.lang.Exception
+import android.app.NotificationManager
+import android.app.NotificationChannel
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import android.media.RingtoneManager
+import android.app.PendingIntent
+import android.content.Context
+import android.net.Uri
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.ExperimentalTransitionApi
+import com.google.accompanist.pager.ExperimentalPagerApi
+import ru.rtuitlab.itlab.R
+import ru.rtuitlab.itlab.ui.MainActivity
 
+@ExperimentalStdlibApi
+@ExperimentalPagerApi
+@ExperimentalAnimationApi
+@ExperimentalTransitionApi
 class AppFirebaseMessagingService : FirebaseMessagingService() {
 	override fun getStartCommandIntent(p0: Intent): Intent {
 		return super.getStartCommandIntent(p0)
@@ -16,11 +33,13 @@ class AppFirebaseMessagingService : FirebaseMessagingService() {
 	}
 
 	override fun onMessageReceived(message: RemoteMessage) {
-		super.onMessageReceived(message)
-		Log.v("Firebase", "Message received")
-		for ((key, value) in message.data) {
-			Log.v("Firebase", "$key : $value")
+		Log.v("Firebase", "Message notification contents: ${message.notification}")
+		// Check if message contains a notification payload.
+		message.notification?.let {
+			sendNotification(it.title, it.body)
 		}
+
+		super.onMessageReceived(message)
 	}
 
 	override fun onDeletedMessages() {
@@ -42,6 +61,43 @@ class AppFirebaseMessagingService : FirebaseMessagingService() {
 	 */
 	override fun onNewToken(token: String) {
 		super.onNewToken(token)
-		Log.v("Firebase", token)
+	}
+
+	private fun sendNotification(title: String?, message: String?) {
+		if (title == null || message == null) {
+			Log.e("Firebase", "Null data provided for notification")
+			return
+		}
+		val intent = Intent(this, MainActivity::class.java)
+		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+		val pendingIntent = PendingIntent.getActivity(
+			this,
+			0,
+			intent,
+			PendingIntent.FLAG_ONE_SHOT
+		)
+		val channelId = getString(R.string.feedback)
+		val defaultSoundUri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+		val notificationBuilder: NotificationCompat.Builder =
+			NotificationCompat.Builder(this, channelId)
+				.setSmallIcon(R.drawable.ic_notification_default)
+				.setContentTitle(title)
+				.setContentText(message)
+				.setAutoCancel(true)
+				.setSound(defaultSoundUri)
+				.setContentIntent(pendingIntent)
+		val notificationManager =
+			getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+		// Since android Oreo notification channel is needed.
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+			val channel = NotificationChannel(
+				channelId,
+				getString(R.string.feedback),
+				NotificationManager.IMPORTANCE_HIGH
+			)
+			notificationManager.createNotificationChannel(channel)
+		}
+		notificationManager.notify(0, notificationBuilder.build())
 	}
 }
