32ec4e7ea5b612314a2e231449a975b0a8f0514b
Alexander <mrapple100@yandex.ru>
2022-12-11 18:12:20 +0300

    DeviceInfoEditSecondaryDialogContent
    to MD3

--- app/src/main/java/ru/rtuitlab/itlab/presentation/screens/devices/components/DeviceInfoEditSecondaryDialogContent.kt
+++ app/src/main/java/ru/rtuitlab/itlab/presentation/screens/devices/components/DeviceInfoEditSecondaryDialogContent.kt
@@ -3,7 +3,8 @@ package ru.rtuitlab.itlab.presentation.screens.devices.components
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -13,6 +14,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import ru.rtuitlab.itlab.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @ExperimentalMaterialApi
 @Composable
 fun DeviceInfoEditSecondaryDialogContent(
@@ -51,11 +53,16 @@ fun DeviceInfoEditSecondaryDialogContent(
                     onValueChange = {
                         description.value = it
                     },
-                    placeholder = { Text(text = hint) },
+                    placeholder = {
+                        Text(
+                            text = hint,
+                            style = MaterialTheme.typography.titleMedium,
+                            color = MaterialTheme.colorScheme.onSurface.copy(.8f)
+                        ) },
                     singleLine = true,
                     colors = TextFieldDefaults.outlinedTextFieldColors(
-                        backgroundColor = MaterialTheme.colors.background,
-                        focusedBorderColor = MaterialTheme.colors.onSurface
+                        containerColor = MaterialTheme.colorScheme.background,
+                        focusedBorderColor = MaterialTheme.colorScheme.onSurface
 
                     ),
                     modifier = Modifier
@@ -73,7 +80,9 @@ fun DeviceInfoEditSecondaryDialogContent(
                         text = stringResource(id = R.string.to_choose),
                         modifier = Modifier.clickable {
                             onConfirm(description.value)
-                        }
+                        },
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.onSurface.copy(.8f),
 
                     )
                 }
