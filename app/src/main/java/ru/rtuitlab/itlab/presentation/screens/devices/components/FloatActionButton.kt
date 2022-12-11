756cc828a3bd7e49852a141e0fc6ee5ac64fe12f
Alexander <mrapple100@yandex.ru>
2022-12-11 18:12:55 +0300

    FloatActionButton
    to MD3

--- app/src/main/java/ru/rtuitlab/itlab/presentation/screens/devices/components/FloatActionButton.kt
+++ app/src/main/java/ru/rtuitlab/itlab/presentation/screens/devices/components/FloatActionButton.kt
@@ -1,23 +1,14 @@
 package ru.rtuitlab.itlab.presentation.screens.devices.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.FloatingActionButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import ru.rtuitlab.itlab.R
 import ru.rtuitlab.itlab.presentation.screens.devices.DevicesViewModel
+import ru.rtuitlab.itlab.presentation.ui.components.bottom_app_bar.ITLabBottomBarDefaults
 import ru.rtuitlab.itlab.presentation.ui.components.bottom_sheet.BottomSheetViewModel
 import ru.rtuitlab.itlab.presentation.utils.AppBottomSheet
 
@@ -30,34 +21,22 @@ fun FloatActionButton(
 ) {
         val coroutineScope = rememberCoroutineScope()
 
-        Column(
-                modifier = Modifier
-                        .fillMaxSize(),
-                verticalArrangement = Arrangement.Bottom,
-                horizontalAlignment = Alignment.End
-        ){
                 FloatingActionButton(
-                        backgroundColor = colorResource(R.color.accent),
-                        elevation = FloatingActionButtonDefaults.elevation(8.dp),
+                        containerColor = ITLabBottomBarDefaults.secondaryFloatingActionButtonContainerColor,
+                        elevation = ITLabBottomBarDefaults.floatingActionButtonsElevation,
                         onClick = {
-
                                 bottomSheetViewModel.show(
                                         AppBottomSheet.DeviceNew(devicesViewModel,bottomSheetViewModel),
                                         coroutineScope)
-
                         }
-                ){
+                ) {
                         Icon(
-                                Icons.Filled.Add,
-                                contentDescription = stringResource(R.string.add_device),
-                                modifier = Modifier
-                                        .width(30.dp)
-                                        .height(22.5.dp)
-
+                                imageVector = Icons.Default.Add,
+                                contentDescription = null
                         )
-
                 }
-        }
+
+
 
 
 
